7f3442f89823c4fe0f0ac079678122a31709539c
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 15:58:39 +0300

    Declare module dependencies bootique/bootique#203

--- src/main/java/io/bootique/linkrest/LinkRestModuleProvider.java
+++ src/main/java/io/bootique/linkrest/LinkRestModuleProvider.java
@@ -3,18 +3,32 @@ package io.bootique.linkrest;
 import com.google.inject.Module;
 import io.bootique.BQModule;
 import io.bootique.BQModuleProvider;
+import io.bootique.cayenne.CayenneModuleProvider;
+import io.bootique.jersey.JerseyModuleProvider;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
 
 public class LinkRestModuleProvider implements BQModuleProvider {
 
-	@Override
-	public Module module() {
-		return new LinkRestModule();
-	}
+    @Override
+    public Module module() {
+        return new LinkRestModule();
+    }
 
-	@Override
-	public BQModule.Builder moduleBuilder() {
-		return BQModuleProvider.super
-				.moduleBuilder()
-				.description("Provides integration with LinkRest framework.");
-	}
+    @Override
+    public BQModule.Builder moduleBuilder() {
+        return BQModuleProvider.super
+                .moduleBuilder()
+                .description("Provides integration with LinkRest framework.");
+    }
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return asList(
+                new CayenneModuleProvider(),
+                new JerseyModuleProvider()
+        );
+    }
 }
--- src/test/java/io/bootique/linkrest/LinkRestModuleProviderTest.java
+++ src/test/java/io/bootique/linkrest/LinkRestModuleProviderTest.java
@@ -1,17 +1,37 @@
 package io.bootique.linkrest;
 
+import io.bootique.BQRuntime;
+import io.bootique.cayenne.CayenneModule;
+import io.bootique.jersey.JerseyModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static com.google.common.collect.ImmutableList.of;
 
 public class LinkRestModuleProviderTest {
 
-	@Test
-	public void testPresentInJar() {
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
+
+    @Test
+    public void testPresentInJar() {
         BQModuleProviderChecker.testPresentInJar(LinkRestModuleProvider.class);
     }
 
-	@Test
-	public void testMetadata() {
-		BQModuleProviderChecker.testMetadata(LinkRestModuleProvider.class);
-	}
+    @Test
+    public void testMetadata() {
+        BQModuleProviderChecker.testMetadata(LinkRestModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new LinkRestModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+                CayenneModule.class,
+                JerseyModule.class,
+                LinkRestModule.class
+        ));
+    }
 }
